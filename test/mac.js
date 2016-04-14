8376181b76f28ac36e87470bbdd703fac5a7f61d
mh-cbon <mh-cbon@users.noreply.github.com>
2016-04-14 14:19:14 +0200

    fix tests

--- test/mac.js
+++ test/mac.js
@@ -3,7 +3,7 @@ var should = require('should')
 describe('hasLaunchd', function() {
   it('returns true on macos', function(done) {
     require('../index.js')(function (err, launchdPath) {
-      (err===undefined).should.be.true;
+      (err===undefined).should.eql(true);
       (launchdPath).should.match(/\/launchd$/);
       done();
     })
--- test/not-mac.js
+++ test/not-mac.js
@@ -3,8 +3,8 @@ var should = require('should')
 describe('hasLaunchd', function() {
   it('returns false on non-macos', function(done) {
     require('../index.js')(function (err, launchdPath) {
-      (err===undefined).should.be.false;
-      (launchdPath===undefined).should.be.true;
+      (err===undefined).should.eql(false);
+      (launchdPath===undefined).should.eql(true);
       done();
     })
   });
